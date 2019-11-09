0139a4f605cbd4a5d9a3cf2be48d142f1845a6be
Aimi Ross <touyamaaimi922@gmail.com>
2019-11-09 14:39:26 -0800

    Initial Commit

---
+++ Student/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Student
+//
+//  Created by AIMI ROSS on 11/9/19.
+//  Copyright © 2019 AIMI ROSS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
